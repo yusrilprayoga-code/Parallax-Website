661a977f284a0ba53938bb3feabe4a3bef4398de
MohYusrilprayoga <124210006@student.upnyk.ac.id>
2023-08-01 20:45:09 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let hill2 = document.getElementById("hill2");
+let hill1 = document.getElementById("hill1");
+let hill4 = document.getElementById("hill4");
+let hill5 = document.getElementById("hill5");
+let leaf = document.getElementById("leaf");
+let text = document.getElementById("text");
+
+window.addEventListener("scroll", () => {
+    let value = window.scrollY;
+    
+    text.style.marginTop = value * 2.5 + "px";
+    leaf.style.top = value * -1.5 + "px";
+    leaf.style.left = value * 1.5 + "px";
+    hill5.style.left = value * 1.5 + "px";
+    hill4.style.left = value * -1.5 + "px";
+    hill2.style.left= value * -0.1 + "px";
+    hill1.style.top= value * -0.1 + "px";
+});
+
